3cb2267721532f57d483b1313fbe808dc4461ebb
dozyhermit <jahryngalbraith@gmail.com>
2025-03-02 15:21:35 +1100

    Fixing eslint, package.json, github workflow

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import { MiddlewareFunctionProps } from '@rescale/nemo';
 import { NextResponse } from 'next/server';
 import { SafeParseReturnType, z } from 'zod';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ZodError = z.SafeParseError<Record<string, any>>;
 
 export type Schema<T> = z.ZodObject<
@@ -16,6 +17,7 @@ export type ValidateReturnType = NextResponse | Response;
 export type ValidateWithSchema<T> = {
   schema: Schema<T>;
   errorHandler?: (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     validationResult: SafeParseReturnType<Record<string, any>, T>
   ) => ValidateReturnType;
 };
