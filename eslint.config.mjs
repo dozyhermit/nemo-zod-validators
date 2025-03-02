350d57452f5133e72efae83f4ac42c934ffa3564
dozyhermit <me@dozyhermit.com>
2025-03-02 15:21:35 +1100

    Fixing eslint, package.json, github workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,8 @@
-export default [{}];
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+  files: ['src/**/*.ts'],
+  ignores: ['src/__tests__/**/*'],
+  extends: [eslint.configs.recommended, tseslint.configs.recommended],
+});
