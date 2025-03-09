32ed18b1ee79ae43ca76856b78dfe7e10f905000
dozyhermit <dozyhermit@users.noreply.github.com>
2025-03-09 14:24:06 +1100

    Updating ValidateWithSchema types to support ZodEffects typed schemas, adding new validateGeneric error handler and control with errorHandlerBuiltIn (#5)

--- src/validateGeneric.ts
+++ src/validateGeneric.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from 'next/server';
-import { errorHandlerWithSchema } from './errorHandler';
+import {
+  errorHandlerWithSchema,
+  errorHandlerWithSchemaFormErrors,
+} from './errorHandler';
 import { ValidateWithSchema, type ValidateReturnType } from './types';
 
 type ValidateGeneric<T> = { data: T } & ValidateWithSchema<T>;
@@ -10,17 +13,23 @@ type ValidateGeneric<T> = { data: T } & ValidateWithSchema<T>;
  * @param {T} data The data to be validated
  * @param {z.ZodObject} schema The schema doing the validating
  * @param {Function} errorHandler A custom error function for custom error return results
+ * @param {string} errorHandlerBuiltIn There are two built-in error handlers for validateGeneric and you can use this to switch between them. The options are `fieldErrors` and `formErrors`, with `fieldErrors` being the default
  */
 export const validateGeneric = <T>({
   data,
   schema,
   errorHandler,
+  errorHandlerBuiltIn = 'fieldErrors',
 }: ValidateGeneric<T>): ValidateReturnType => {
   const validationResult = schema.safeParse(data as T);
 
   if (validationResult.error) {
     if (errorHandler) {
       return errorHandler(validationResult);
+    }
+
+    if (errorHandlerBuiltIn === 'formErrors') {
+      return errorHandlerWithSchemaFormErrors(validationResult);
     }
 
     return errorHandlerWithSchema(validationResult);
