7b29f1278f9a5a517016e3c35d23cce755705048
dozyhermit <jahryngalbraith@gmail.com>
2025-03-09 13:24:57 +1100

    Updating ValidateWithSchema types to support ZodEffects typed schemas, adding new validateGeneric error handler and control with errorHandlerBuiltIn

--- src/__tests__/mocks/schema.ts
+++ src/__tests__/mocks/schema.ts
@@ -37,3 +37,38 @@ export const cookieSchema = z.object({
 });
 
 export type CookieSchemaType = z.infer<typeof cookieSchema>;
+
+export const schemaWithZodEffects = z
+  .object({
+    Number: z.number({
+      invalid_type_error: 'Invalid Number type',
+    }),
+    String: z
+      .string({
+        invalid_type_error: 'Invalid String type',
+      })
+      .optional(),
+    AdvancedString: z
+      .string({
+        invalid_type_error: 'Invalid AdvancedString type',
+      })
+      .regex(/^[0-9]+$/, 'Invalid AdvancedString regex'),
+  })
+  .partial()
+  .superRefine((data, ctx) => {
+    if (!data.Number && !data.AdvancedString) {
+      ctx.addIssue({
+        path: ['Number'],
+        code: z.ZodIssueCode.custom,
+        message: 'Must have at least one of Number or AdvancedString',
+      });
+
+      ctx.addIssue({
+        path: ['AdvancedString'],
+        code: z.ZodIssueCode.custom,
+        message: 'Must have at least one of AdvancedString or Number',
+      });
+    }
+  });
+
+export type SchemaWithZodEffectsType = z.infer<typeof schemaWithZodEffects>;
--- src/__tests__/validateGeneric.test.ts
+++ src/__tests__/validateGeneric.test.ts
@@ -2,8 +2,10 @@ import { validateGeneric } from '../validateGeneric';
 import { errorHandler } from './mocks/errorHandler';
 import {
   type SchemaType,
+  SchemaWithZodEffectsType,
   schema,
   schemaWithIntentionallyBrokenSafeParse,
+  schemaWithZodEffects,
 } from './mocks/schema';
 
 describe('validateGeneric', () => {
@@ -54,6 +56,52 @@ describe('validateGeneric', () => {
       });
 
       expect(await result.json()).toMatchSnapshot();
+    });
+  });
+
+  describe('with errorHandlerBuiltIn as formErrors', () => {
+    test('passes validation with correct data', () => {
+      const result = validateGeneric<SchemaType>({
+        data: { Number: 123, String: 'Hello', AdvancedString: '123456' },
+        schema,
+        errorHandlerBuiltIn: 'formErrors',
+      });
+
+      expect(result.status).toBe(200);
+    });
+
+    test('triggers validation error with incorrect data', async () => {
+      const result = validateGeneric<SchemaType>({
+        data: { Number: 123, String: 'Hello', AdvancedString: 'World' },
+        schema,
+        errorHandlerBuiltIn: 'formErrors',
+      });
+
+      expect(await result.json()).toMatchSnapshot();
+    });
+
+    test('triggers validation error with incorrect data and custom error handler', async () => {
+      const result = validateGeneric<SchemaType>({
+        data: { Number: 123, String: 'Hello', AdvancedString: 'World' },
+        schema,
+        errorHandler,
+        errorHandlerBuiltIn: 'formErrors',
+      });
+
+      expect(await result.json()).toMatchSnapshot();
+    });
+  });
+
+  // This is just testing that the types definitions work okay with a ZodEffects style schema.
+  // We don't really need to test that this works/doesn't work.
+  describe('with schema that has one of two required fields', () => {
+    test('passes validation with at least one the required fields', async () => {
+      const result = validateGeneric<SchemaWithZodEffectsType>({
+        data: { String: 'Hello', Number: 12 },
+        schema: schemaWithZodEffects,
+      });
+
+      expect(result.status).toBe(200);
     });
   });
 });
