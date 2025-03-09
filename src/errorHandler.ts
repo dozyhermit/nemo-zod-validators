237fccb6500bb5889bb8e0da8bed18d91bf793ed
dozyhermit <me@dozyhermit.com>
2025-03-09 14:24:06 +1100

    Updating ValidateWithSchema types to support ZodEffects typed schemas, adding new validateGeneric error handler and control with errorHandlerBuiltIn (#5)

--- src/errorHandler.ts
+++ src/errorHandler.ts
@@ -6,11 +6,20 @@ export const errorHandlerWithSchema = ({ error }: ZodError) =>
   Response.json(
     {
       message:
-        // this seems ridiculous and it is, sorry
         (error?.flatten &&
-        error?.flatten()?.fieldErrors &&
-        Object.values(error?.flatten()?.fieldErrors).length >= 1
+        Object.values(error?.flatten()?.fieldErrors || {}).length >= 1
           ? error?.flatten()?.fieldErrors
+          : DEFAULT_ERROR_MESSAGE) || DEFAULT_ERROR_MESSAGE,
+    },
+    { status: 422 }
+  );
+
+export const errorHandlerWithSchemaFormErrors = ({ error }: ZodError) =>
+  Response.json(
+    {
+      message:
+        (error?.flatten && (error?.flatten()?.formErrors || []).length >= 1
+          ? error?.flatten()?.formErrors
           : DEFAULT_ERROR_MESSAGE) || DEFAULT_ERROR_MESSAGE,
     },
     { status: 422 }
